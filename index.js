f3d8e482182e0a4333f1eac2da2701ba633278e1
Anthony Tran <anthonytran.dev@gmail.com>
2017-10-02 14:55:24 +0100

    Integrated Sliding Feature with Terminal Template

--- index.js
+++ index.js
@@ -18,10 +18,12 @@
 
 const projectShow1 = document.getElementById("project-show-1");
 const panelSlide1 = document.getElementById("panel-slide-out-1");
+const panelContent = document.getElementById("panel-content");
 const cursor = document.getElementById("cursor");
 const cursor2 = document.getElementById("cursor-2");
 const terminalLine1 = document.getElementById("terminal-line-1");
 const terminalLine2 = document.getElementById("terminal-line-2");
+const underneath = document.getElementsByClassName("underneath");
 let animate;
 let animate1;
 let animate2;
@@ -55,6 +57,78 @@ const terminalMessage = {
     "line20": ""
 }
 
+const googleCalcMessage = {
+    "line0": "",
+    "line1": "CITADEL OF RICKS UNIFIED OPERATING SYSTEM",
+    "line2": "COPYRIGHT 2017 ANTCO INDUSTRIES",
+    "line3": "-Server 23-",
+    "line4": "",
+    "line5": "Space PortX Control Network",
+    "line6": "_______________________________",
+    "line7": "",
+    "line8": "Progress: [=========-------] 56% Alpha 0.1.0",
+    "line9": "Last Updated: 01/10/2017",
+    "line10": "Skills Gained:",
+    "line11": " - Unit testing; Mocha + Chai",
+    "line12": " - Integration testing of multiple features",
+    "line13": "",
+    "line14": "",
+    "line15": "",
+    "line16": "",
+    "line17": "",
+    "line18": '<a target="_blank" style="color:rgb(64, 227, 156)" href="https://developer.mozilla.org/en-US/docs/Web/CSS/animation">[Project Page Link]</a> ---- <a target="_blank" style="color:rgb(64, 227, 156)" href="https://github.com/anthonytranDev/JavaScript_Calculator">[Github Page]</a>',
+    "line19": "",
+    "line20": ">"
+}
+
+const googleCalcMargins = {
+    "line0": "",
+    "line1": "8/42",
+    "line2": "11/42",
+    "line3": "18/42",
+    "line4": "2/42",
+    "line5": "2/42",
+    "line6": "2/42",
+    "line7": "2/42",
+    "line8": "2/42",
+    "line9": "2/42",
+    "line10": "2/42",
+    "line11": "2/42",
+    "line12": "2/42",
+    "line13": "2/42",
+    "line14": "2/42",
+    "line15": "2/42",
+    "line16": "2/42",
+    "line17": "2/42",
+    "line18": "2/42",
+    "line19": "2/42",
+    "line20": "2/42"
+}
+
+const emptyMessage = {
+    "line0": "",
+    "line1": "",
+    "line2": "",
+    "line3": "",
+    "line4": "",
+    "line5": "",
+    "line6": "",
+    "line7": "",
+    "line8": "",
+    "line9": "",
+    "line10": "",
+    "line11": "",
+    "line12": "",
+    "line13": "",
+    "line14": "",
+    "line15": "",
+    "line16": "",
+    "line17": "",
+    "line18": "",
+    "line19": "",
+    "line20": ""
+}
+
 const defaultMargins = {
     "line0": "",
     "line1": "2/42",
@@ -80,14 +154,13 @@ const defaultMargins = {
 }
 
 
-const insertTerminalText = (node, message, margins = defaultMargins) => {
+const insertTerminalText = (node, message, margins) => {
         // let isNotNode = new Promise;
         const arr = [];
-
         for (let i = 0; i < Object.values(message).length; i++) {
-            if (terminalMessage[`line${i}`] !== "") {
+            if (message[`line${i}`] !== "") {
                 node.innerHTML +=
-                    `<div id="terminal-line-${i}" class="underneath">${terminalMessage[`line${i}`]}</div>`
+                    `<div id="terminal-line-${i}" class="underneath">${message[`line${i}`]}</div>`
                     arr.push(i);
                 }
             }
@@ -97,75 +170,48 @@ const insertTerminalText = (node, message, margins = defaultMargins) => {
                     document.getElementById(`terminal-line-${j}`).style["grid-column"] = margins[`line${j}`];
                 }
             }
-            let promise = new Promise((resolve, reject)=>{
+            const generateTerminalDivs = new Promise((resolve, reject)=>{
                 if(arr.length){
                     resolve()
                 }else{
                     reject(Error("arr length of 13 was never reached"))
                 }
             });
-            promise.then(
+            generateTerminalDivs.then(
                 ()=>{
                     changeGridColumnConfig();
                 }
             ), (error) => {
                 console.log(error);
             }
+            node.innerHTML += `<div id="cursor" style="grid-column: 3/42; grid-row: 40/42" class="blinking"></div>`
+        }
+
+projectShow1.addEventListener("click",
+()=> {
+    if (panelSlide1.style.height == "100%") {
+        panelSlide1.style.animation = "0.5s ease-in slideinheight";
+        panelContent.style.visibility = "hidden";
+        panelSlide1.style.height = "";
+        
+    } else {
+        panelSlide1.style.animation = "1s slideoutheight";
+            panelSlide1.style.height = "100%";
+            panelContent.style.visibility = "hidden";
+        if(panelContent.innerHTML.length === 0){
+            insertTerminalText(panelContent, googleCalcMessage, googleCalcMargins)
+        }
+        if(panelContent.innerHTML.length !== 0){
+            // panelContent.style.animation = "0.5s ease-in 1s fadein";
+            setTimeout(()=>{panelContent.style.visibility = "visible";}, 1000);
+            }
+        }
     }
-
-
-insertTerminalText(panelSlide1, terminalMessage);
-
-// projectShow1.addEventListener("click",
-//     function() {
-//         if (panelSlide1.style.height == "100%") {
-//             panelSlide1.style.animation = "1s slideinheight";
-//             panelSlide1.style.height = "";
-//             cursor.style.visibility = "hidden";
-//             terminalLine1.style.visibility = "hidden";
-//             cursor2.style.visibility = "hidden";
-//             terminalLine2.style.visibility = "hidden";
-//         } else {
-//             panelSlide1.style.animation = "1s slideoutheight";
-//             panelSlide1.style.height = "100%"
-//             panelSlide1.style.height = "100%";
-//             animate = setTimeout(() => {
-//                 cursor.style.backgroundColor = "rgb(49, 254, 131)";
-//                 cursor.style.animation = "1s blinkmove linear";
-//                 terminalLine1.style.color = "rgb(64, 227, 156)"
-//                 terminalLine1.style.animation = "type 1s steps(60, end)";
-//             }, 1000);
-//             animation1 = setTimeout(() => {
-//                 cursor.style.animation = "1s blink infinite";
-//                 cursor.style.left = "500px";
-//             }, 2000);
-//             animation2 = setTimeout(() => {
-//                 cursor.style.visibility = "hidden";
-//                 cursor2.style.animation = "1s blinkmove2 linear";
-//                 terminalLine2.style.animation = "type 1s steps(60, end)";
-//             }, 3000);
-//             animation3 = setTimeout(() => {
-//                 terminalLine2.style.color = "rgb(64, 227, 156)";
-//                 cursor2.style.animation = "1s blink infinite";
-//                 cursor2.style.left = "435px";
-
-//             }, 4000);
-//         }
-//     }
-
-// )
-// panelSlide1.addEventListener("click",
-//     () => {
-//         clearTimeout(animation);
-//         clearTimeout(animation1);
-//         clearTimeout(animation2);
-//         clearTimeout(animation3);
-//         cursor.style.visibility = "hidden";
-//         terminalLine1.style.visibility = "hidden";
-//         cursor2.style.visibility = "hidden";
-//         terminalLine2.style.visibility = "hidden";
-//         // panelSlide1.style.animation = "1s slideoutheight";
-//         panelSlide1.style.height = "";
-//         animation = animateObj.stop();
-//     }
-// )
+)
+panelSlide1.addEventListener("click",
+() => {
+    panelSlide1.style.animation = "0.5s ease-in slideinheight";
+    panelContent.style.visibility = "hidden";
+    panelSlide1.style.height = "";
+}
+)
