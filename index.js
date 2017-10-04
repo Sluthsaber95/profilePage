b21a14d21e6a1951dcf29ae7897e29c07a41669b
Anthony Tran <anthonytran.dev@gmail.com>
2017-10-04 01:00:13 +0100

    Feature Intergration: Graph Finished

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-(function() {
+(() => {
 
-    var parallax = document.querySelectorAll(".parallax"),
+    const parallax = document.querySelectorAll(".parallax"),
         speed = 0.4;
 
-    window.onscroll = function() {
-        [].slice.call(parallax).forEach(function(el, i) {
+    window.onscroll = () => {
+        [].slice.call(parallax).forEach((el, i) => {
 
-            var windowYOffset = window.pageYOffset,
+            let windowYOffset = window.pageYOffset,
                 elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
 
             el.style.backgroundPosition = elBackgrounPos;
