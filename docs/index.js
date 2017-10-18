5044c80c332a02e3ab393dc5cdd3050f85463499
Anthony Tran <anthonytran.dev@gmail.com>
2017-10-18 01:10:10 +0100

    Included a Brand Theme Icons
    Included Brand Icons

--- docs/index.js
+++ docs/index.js
@@ -26,6 +26,7 @@
 var projectImg = document.getElementsByClassName("project-img");
 var projects = document.getElementsByClassName("projects");
 var projectBtn = document.getElementsByClassName("project-btn");
+var companyIcon = document.getElementsByClassName("company-icon");
 
 // targets a project, via zero-index and changes message if required
 var terminalMessage = {
@@ -38,16 +39,31 @@ var terminalMessage = {
     "6": ["", "ERROR 0XFFFFF710", 'Processor', 'Corr:xsfkleg..g364[7345}4__."', "_______________________________", "", "", "", "", '', ">"],
     "7": ["", "ERROR 0XFFFFF710", 'Processor', 'Corr:xsfkleg..g364[7345}4__."', "_______________________________", "", "", "", "", '', ">"]
 };
-// listens for projects being clicked in the 'project-section'
+// include the right links with the accompanying image and message
+var projectLinks = {
+        "0": "http://clerk-dolphin-85063.bitballoon.com/",
+        "1": "http://gardener-kevin-53240.bitballoon.com/",
+        "2": "",
+        "3": "",
+        "4": "",
+        "5": "",
+        "6": "",
+        "7": ""
+    }
+    // listens for projects being clicked in the 'project-section'
 projects[0].addEventListener('click', function(el) {
 
     // used to create a child div => containing  terminal message
     function messageTranscriber(node, terminalMessage, classIndex) {
         // creates <div id="terminal-text-[index]" style="position: absolute"></div>
         var div = document.createElement("div");
+        const viewPortWidth = Math.min(document.documentElement.clientWidth, window.innerWidth);
         div.id = "terminal-text-" + classIndex;
+        whiteCover.style.width = 60 + scrollBox.scrollTop / 3 + "px";
         div.style.position = "absolute";
-
+        div.style.top = 0;
+        div.style.position = "absolute";
+        div.style.top = 0;
         for (var i = 0; i < 10; i++) {
             div.innerHTML += "<div class=\"terminal-text\">" + terminalMessage[i] + "</div>";
         }
@@ -58,21 +74,42 @@ projects[0].addEventListener('click', function(el) {
     // gives an index, used to identify which element is being pressed
     var i = el.target.id.slice(-1);
 
-    if (projectImg[i].style.opacity == "0") {
-        // project image reappeats
-        projectImg[i].style.animation = "0.25s fadein";
+    // two conditions are used here, `projectImg[i] & projectImg[i].style.opacity == "0"`
+    // as a similar if statement for window.scroll event listener used below, 
+    // resorting to projectImg[i] becoming undefined
+    if (projectImg[i] && projectImg[i].style && projectImg[i].style.opacity == "0") {
+        const animationSpeed = 0.5;
+        console.log("shouldn't see this first")
+        projectImg[i].style.animation = `${animationSpeed}fadein`;
         projectImg[i].style.opacity = 1;
+        if (companyIcon[i] & companyIcon[i].style.opacity == "0") {
+            companyIcon[i].style.animation = `${animationSpeed}fadein`;
+            companyIcon[i].style.opacity = 1;
+        }
         // removes child div from within button of index `i`; more precisely 
         // from within {class="projectBtn"} makes the terminal message disappear
         var child = document.getElementById("terminal-text-" + i);
         projectBtn[i].removeChild(child);
+        setTimeout(() => {
+            projectImg[i].style.animation = "";
+            if (companyIcon[i] & companyIcon[i].style.opacity == "0") {
+                companyIcon[i].style.animation = "";
+            };
+        }, animationSpeed * 1000);
     }
     // project image disappears
     // appends child div from within button element, given index `i`; more 
     // precisely from within {class="projectBtn"} makes the terminal message appear
     else {
-        projectImg[i].style.animation = "0.25s fadeout";
+        projectLinks[`${i}`] === "" ?
+            null : window.open(projectLinks[`${i}`]);
+        const animationSpeed = 0.5;
+        projectImg[i].style.animation = `${animationSpeed}fadeout`;
         projectImg[i].style.opacity = 0;
+        if (companyIcon[i]) {
+            companyIcon[i].style.animation = `${animationSpeed}fadeout`;
+            companyIcon[i].style.opacity = 0;
+        }
         projectBtn[i].style.backgroundImage = "url(http://i.imgur.com/IL9nJ3d.png)";
         var terminalMsgMobile = terminalMessage["" + i];
         messageTranscriber(projectBtn[i], terminalMsgMobile, i);
@@ -85,10 +122,38 @@ var whiteCover = document.getElementsByClassName("white-cover")[0];
 // Depending on the size of the screen, this dictates the graph white
 // coverage upon scroll down of scroll-box
 scrollBox.addEventListener("scroll", function() {
-    var viewPortWidth = Math.min(document.documentElement.clientWidth, window.innerWidth);
+    const viewPortWidth = Math.min(document.documentElement.clientWidth, window.innerWidth);
     if (viewPortWidth < 720) {
         whiteCover.style.width = 60 + scrollBox.scrollTop / 3 + "px";
-    } else if (viewPortWidth < 1366) {
+    } else {
         whiteCover.style.width = 140 + scrollBox.scrollTop / 1.5 + "px";
     }
+});
+
+window.addEventListener("scroll", function() {
+    const projectFaded = new Promise((resolve, reject) => {
+        let i = 0
+        for (let i = 0; i < 8; i++) {
+            if (projectImg[i] && projectImg[i].style && projectImg[i].style.opacity === "0") {
+                return resolve(i);
+            }
+        }
+
+    });
+    projectFaded.then((i) => {
+        const animationTime = 0.5;
+        projectImg[i].style.animation = `${animationTime}s fadein`;
+        projectImg[i].style.opacity = 1;
+        companyIcon[i].style.animation = `${animationTime}fadein`;
+        companyIcon[i].style.opacity = 1;
+        setTimeout(() => {
+            projectImg[i].style.animation = "";
+            if (companyIcon[i] & companyIcon[i].style.opacity == "0") {
+                companyIcon[i].style.animation = "";
+            };
+        }, animationTime * 1000);
+        // if (companyIcon[i] && companyIcon[i].style.opacity == "0") {}
+        var child = document.getElementById("terminal-text-" + i);
+        projectBtn[i].removeChild(child);
+    });
 });
